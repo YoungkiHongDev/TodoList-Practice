37949560c51d37be807a0fe9c70c73653a1befd1
YoungkiHongDev <dudrl1410@naver.com>
2023-03-14 05:01:31 +0900

    Service 패키지 TodoService 클래스 추가

---
+++ src/main/java/com/codepresso/todo/service/TodoService.java
@@ -0,0 +1,19 @@
+package com.codepresso.todo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    public String getTodoList() {
+        return null;
+    }
+
+    public String addTodo() {
+        return null;
+    }
+
+    public String deleteTodo() {
+        return null;
+    }
+}
